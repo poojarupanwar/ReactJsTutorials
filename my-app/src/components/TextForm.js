1455ea38b34c927e9f237b7a7f3a824320d4eee0
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-05-21 15:35:00 +0530

    Update TextForm.js

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -24,12 +24,17 @@ export default function TextForm(props) {
     <div className="mb-3">
     <textarea className="form-control" onChange={handleOnChange} id="myBox" rows="8" value={text}></textarea>
     </div>
-    <button className='btn btn-primary' onClick={handleUpClick}>Convert To Uppercase</button> <span></span>
-    <button className='btn btn-primary' onClick={handleLowClick}>Convert To Lowercase</button>
+    <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert To Uppercase</button>  
+    <button className='btn btn-primary mx-1' onClick={handleLowClick}>Convert To Lowercase</button>
     </div>
     <div  className='container my-3'>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length}Minutes to read</p>
+        <div>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
     </div>
     </>
     
