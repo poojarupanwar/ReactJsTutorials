bd2c29ef27033595b7e8e34c7f5338a8255fd41c
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-05-21 23:26:55 +0530

    Update Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -5,23 +5,48 @@ export default function Navbar(props) {
   return (
 
     
-    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
-         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+  /*  <nav classNameName="navbar navbar-expand-lg bg-body-tertiary ">
+        <div classNameName="container-fluid">
+          <a classNameName="navbar-brand" href="/">{props.title}</a>
+         <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
+        <li classNameName="nav-item">
+          <a classNameName="nav-link active" aria-current="page" href="/">{props.home}</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+        <li classNameName="nav-item">
+          <a classNameName="nav-link" href="/">Link</a>
         </li>
             
       </ul>
     
     </div>
   </div>
-  </nav>
+  </nav> */
+
+  <>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="/">{props.title}</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">{props.home}  </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="/">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </>
    
   )
 }
